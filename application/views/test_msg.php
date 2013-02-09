586eaa0e9efa9cf03f43ddf80015de92961aa448
Timothy Boronczyk <tboronczyk@gmail.com>
2013-02-09 14:40:52 -0500

    Initial commit

---
+++ application/views/test_msg.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1>Test!</h1>
+
+	<div id="body">
+		<p>Hello</p>
+	</div>
+
+</div>
+
+</body>
+</html>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to PHPMaster's Fortrabbit demo</title>
+
+	<style type="text/css">
+
+	::selection{ background-color: #E13300; color: white; }
+	::moz-selection{ background-color: #E13300; color: white; }
+	::webkit-selection{ background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
+
+	h1 {
+		color: #444;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 19px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+
+	code {
+		font-family: Consolas, Monaco, Courier New, Courier, monospace;
+		font-size: 12px;
+		background-color: #f9f9f9;
+		border: 1px solid #D0D0D0;
+		color: #002166;
+		display: block;
+		margin: 14px 0 14px 0;
+		padding: 12px 10px 12px 10px;
+	}
+
+	#body{
+		margin: 0 15px 0 15px;
+	}
+	
+	p.footer{
+		text-align: right;
+		font-size: 11px;
+		border-top: 1px solid #D0D0D0;
+		line-height: 32px;
+		padding: 0 10px 0 10px;
+		margin: 20px 0 0 0;
+	}
+	
+	#container{
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		-webkit-box-shadow: 0 0 8px #D0D0D0;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to the Fortrabbit demo app!</h1>
+
+	<div id="body">
+		<p>Hello <?php echo $databaseData; ?>!</p>
+
+		<?php if ((int)$id == 1) : ?>
+		<a href="/welcome/index/id/2" title="Testing alternative Hello">Now try this link.</a>
+		<?php elseif ((int)$id > 1) : ?>
+		<a href="/" title="Back home">Go back home.</a>
+		<?php endif; ?>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
